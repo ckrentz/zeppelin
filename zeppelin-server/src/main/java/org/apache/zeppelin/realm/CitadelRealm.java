a7462687ae15779cd4858bf1c01fc1672f2363cc
Unknown <cpkrentz@gmail.com>
2018-11-20 15:17:03 -0500

    Changes to add Citadel and BDP code to zeppelin

---
+++ zeppelin-server/src/main/java/org/apache/zeppelin/realm/CitadelRealm.java
@@ -0,0 +1,50 @@
+package org.apache.zeppelin.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.util.StringUtils;
+
+public class CitadelRealm  extends AuthorizingRealm {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
+
+        try {
+            //Verify authentication
+        } catch (Exception e) {
+            String msg = StringUtils.clean(e.getMessage());
+            if (msg == null) {
+                msg = StringUtils.clean(e.getLocalizedMessage());
+            }
+            if (msg == null) {
+                msg = "Invalid login or password.";
+            }
+            throw new AuthenticationException(msg, e);
+        }
+
+        PrincipalCollection principals = new SimplePrincipalCollection(token.getPrincipal(), this.getName());
+
+        try {
+
+        } catch (Exception e) {
+            throw new AuthenticationException("Unable to obtain authenticated account properties.", e);
+        }
+
+        return new SimpleAuthenticationInfo(principals, null);
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        PrincipalCollection principals = new SimplePrincipalCollection();
+        try {
+
+        } catch(Throwable t) {
+            System.out.println(t.getMessage());
+        }
+        SimpleAuthorizationInfo authn = new SimpleAuthorizationInfo();
+        return authn;
+    }
+}
