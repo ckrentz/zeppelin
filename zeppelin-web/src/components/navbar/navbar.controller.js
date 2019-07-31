1568b12d40473b6b7fa544a500545bbf5b08f048
Chris Krentz <cpkrentz@gmail.com>
2019-07-31 04:52:20 -0400

    Made login happen on initial Login button press, but still need to remove modal popup

--- zeppelin-web/src/components/navbar/navbar.controller.js
+++ zeppelin-web/src/components/navbar/navbar.controller.js
@@ -16,7 +16,8 @@ angular.module('zeppelinWebApp').controller('NavCtrl', NavCtrl);
 
 function NavCtrl($scope, $rootScope, $http, $routeParams, $location,
                  noteListFactory, baseUrlSrv, websocketMsgSrv,
-                 arrayOrderingSrv, searchService, TRASH_FOLDER_ID) {
+                 arrayOrderingSrv, searchService, TRASH_FOLDER_ID,
+                 $httpParamSerializer, $timeout) {
   'ngInject';
 
   let vm = this;
@@ -191,9 +192,10 @@ function NavCtrl($scope, $rootScope, $http, $routeParams, $location,
   }
 
   function showLoginWindow() {
-    setTimeout(function() {
-      angular.element('#userName').focus();
-    }, 500);
+    $scope.login();
+    // setTimeout(function() {
+    //   angular.element('#userName').focus();
+    // }, 500);
   }
 
   /*
@@ -257,4 +259,102 @@ function NavCtrl($scope, $rootScope, $http, $routeParams, $location,
   $rootScope.isRevisionSupported = function() {
     return revisionSupported;
   };
+
+  $scope.SigningIn = false;
+  $scope.loginParams = {};
+
+  let initValues = function() {
+    let req = new XMLHttpRequest();
+    req.open('GET', document.location, false);
+    req.send(null);
+    let headers = req.getAllResponseHeaders();
+
+    let arr = headers.trim().split(/[\r\n]+/);
+    let headerMap = {};
+    arr.forEach(function(line) {
+      let parts = line.split(': ');
+      let header = parts.shift();
+      let value = parts.join(': ');
+      headerMap[header] = value;
+    });
+    console.log('Username: ', headerMap['x-user-id']);
+
+    $scope.loginParams = {
+      userName: headerMap['x-user-id'],
+      password: headerMap['x-user-id'],
+    };
+  };
+
+  initValues();
+  $scope.login = function() {
+    console.log('Starting login... ');
+    $scope.SigningIn = true;
+    $http({
+      method: 'GET',
+      url: baseUrlSrv.getRestApiBase() + '/login',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: $httpParamSerializer({
+        'userName': $scope.loginParams.userName,
+        'password': $scope.loginParams.password,
+      }),
+    }).then(function successCallback(response) {
+      $rootScope.ticket = response.data.body;
+      angular.element('#loginModal').modal('toggle');
+      $rootScope.$broadcast('loginSuccess', true);
+      $rootScope.userName = $scope.loginParams.userName;
+      $scope.SigningIn = false;
+
+      // redirect to the page from where the user originally was
+      if ($location.search() && $location.search()['ref']) {
+        $timeout(function() {
+          let redirectLocation = $location.search()['ref'];
+          $location.$$search = {};
+          $location.path(redirectLocation);
+        }, 100);
+      }
+    }, function errorCallback(errorResponse) {
+      console.log(errorResponse);
+      $scope.loginParams.errorText = 'User has not been granted access to Zeppelin: '
+        + $scope.loginParams.userName;
+      $scope.SigningIn = false;
+    });
+  };
+
+  // handle session logout message received from WebSocket
+  $rootScope.$on('session_logout', function(event, data) {
+    if ($rootScope.userName !== '') {
+      $rootScope.userName = '';
+      $rootScope.ticket = undefined;
+
+      setTimeout(function() {
+        $scope.loginParams = {};
+        $scope.loginParams.errorText = data.info;
+        angular.element('.nav-login-btn').click();
+      }, 1000);
+      let locationPath = $location.path();
+      $location.path('/').search('ref', locationPath);
+    }
+  });
+
+  window.onload = function() {
+    // alert('Trying login now');
+    $scope.login();
+  };
+
+  /*
+   ** $scope.$on functions below
+   */
+  $scope.$on('initLoginValues', function() {
+    initValues();
+  });
+
+  $scope.$on('login', function() {
+    $scope.login();
+  });
+
+  $rootScope.$on('login', function() {
+    $scope.login();
+  });
 }
